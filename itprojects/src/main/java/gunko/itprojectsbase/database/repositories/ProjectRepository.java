42731ac5ce89c18c5dcadb84d34fccf8c3a5a435
Dmitriy Hunko <47152025+Katran88@users.noreply.github.com>
2020-12-10 22:06:57 +0300

    jwt, rest open api, junit

--- itprojects/src/main/java/gunko/itprojectsbase/database/repositories/ProjectRepository.java
+++
@@ -1,6 +0,0 @@
-package gunko.itprojectsbase.database.repositories;
-
-import gunko.itprojectsbase.database.Project;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProjectRepository extends CrudRepository<Project, Integer> {}
---
+++ itprojects/src/main/java/spring/security/jwt/validator/ProjectValidator.java
@@ -0,0 +1,23 @@
+package spring.security.jwt.validator;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import spring.security.jwt.bean.Project;
+import spring.security.jwt.bean.User;
+@Component
+public class ProjectValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return User.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Project DoctorDocument =(Project)o;
+        if(DoctorDocument.getId()<0){
+            errors.rejectValue("id","negative value");
+        }
+    }
+}
